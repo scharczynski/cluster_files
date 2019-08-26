e88f59d7e3acbb5023dffe5f990bda62bf990392
scharczynski <scharcz@brandeis.edu>
2019-08-26 18:19:47 -0400

    setup for jay ca1:

--- run_cell.py
+++ run_cell.py
@@ -449,18 +449,68 @@ def run_script(cell_range, session):
     # pipeline.compare_even_odd("Const", "Time", 0.001)
 
     # path_to_data = '/Users/stevecharczynski/workspace/data/brincat_miller'
-    path_to_data = "/projectnb/ecog-eeg/stevechar/data/brincat_miller/"
-    time_info = [500, 1750]
+    # path_to_data = "/projectnb/ecog-eeg/stevechar/data/brincat_miller/"
+    # time_info = [500, 1750]
+    # # save_dir = "/Users/stevecharczynski/Desktop/test/"
+    # save_dir = "/projectnb/ecog-eeg/stevechar/ml_output/brincat_miller/"
+    # # path_to_data = "/projectnb/ecog-eeg/stevechar/data/sheehan/all_sessions/{0}".format(session)
+
+    # # time_info = list(zip(np.zeros(len(trial_length), dtype=int), trial_length))
+    # data_processor = analysis.DataProcessor(
+    #     path_to_data, cell_range, window=time_info)
+    # n_t = 2.
+    # solver_params = {
+    #     "niter": 5,
+    #     "stepsize": 1000,
+    #     "interval": 10,
+    #     "method": "TNC",
+    #     "use_jac": True,
+    #     "T" : 1,
+    #     "disp":False
+    # }
+    # mean_delta = 0.10 * (time_info[1] - time_info[0])
+    # mean_bounds = (
+    #     (time_info[0] - mean_delta),
+    #     (time_info[1] + mean_delta))
+    # n = 2
+    # bounds_t = {
+    #     "a_1": [0, 1 / n],
+    #     "ut": [mean_bounds[0], mean_bounds[1]],
+    #     "st": [10, 1000],
+    #     "a_0": [10**-10, 1 / n]
+    # }
+    # bounds_c = {
+    #     "a_0": [10**-10, 1 / n]
+    # }
+    # pipeline = analysis.Pipeline(cell_range, data_processor, [
+    #     "Const", "Time"], save_dir=save_dir)
+    # # pipeline = analysis.Pipeline(cell_range, data_processor, [
+    # #     "ConstVariable", "RelPosVariable"], save_dir=save_dir)
+
+    # # pipeline.set_model_bounds("TimeVariableLength", bounds_t)
+    # pipeline.set_model_bounds("Const", bounds_c)
+    # pipeline.set_model_bounds("Time", bounds_t)
+    # pipeline.set_model_x0("Time", [1e-5, 700, 300, 1e-5])
+    # pipeline.set_model_x0("Const", [1e-5])
+    # # pipeline.show_rasters()
+    # pipeline.fit_even_odd(solver_params=solver_params)
+    # pipeline.fit_all_models(solver_params=solver_params)
+    # pipeline.compare_even_odd("Const", "Time", 0.01)
+    # # pipeline.compare_even_odd("Const", "Time", 0.01)
+    # pipeline.compare_models("Const", "Time", 0.01, smoother_value=100)
+
+    # path_to_data = "/Users/stevecharczynski/workspace/data/jay/ca1_time_cell_data/"
+    path_to_data = "/projectnb/ecog-eeg/stevechar/data/jay/ca1_time_cells"
     # save_dir = "/Users/stevecharczynski/Desktop/test/"
-    save_dir = "/projectnb/ecog-eeg/stevechar/ml_output/brincat_miller/"
+    save_dir = "/projectnb/ecog-eeg/stevechar/ml_output/jay/ca1_time_cells/"
     # path_to_data = "/projectnb/ecog-eeg/stevechar/data/sheehan/all_sessions/{0}".format(session)
 
     # time_info = list(zip(np.zeros(len(trial_length), dtype=int), trial_length))
     data_processor = analysis.DataProcessor(
-        path_to_data, cell_range, window=time_info)
+        path_to_data, cell_range)
     n_t = 2.
     solver_params = {
-        "niter": 5,
+        "niter": 100,
         "stepsize": 1000,
         "interval": 10,
         "method": "TNC",
@@ -468,14 +518,10 @@ def run_script(cell_range, session):
         "T" : 1,
         "disp":False
     }
-    mean_delta = 0.10 * (time_info[1] - time_info[0])
-    mean_bounds = (
-        (time_info[0] - mean_delta),
-        (time_info[1] + mean_delta))
     n = 2
     bounds_t = {
         "a_1": [0, 1 / n],
-        "ut": [mean_bounds[0], mean_bounds[1]],
+        "ut": [-1000, 9000],
         "st": [10, 1000],
         "a_0": [10**-10, 1 / n]
     }
@@ -483,22 +529,21 @@ def run_script(cell_range, session):
         "a_0": [10**-10, 1 / n]
     }
     pipeline = analysis.Pipeline(cell_range, data_processor, [
-        "Const", "Time"], save_dir=save_dir)
+        "ConstVariable", "TimeVariableLength"])
     # pipeline = analysis.Pipeline(cell_range, data_processor, [
     #     "ConstVariable", "RelPosVariable"], save_dir=save_dir)
 
     # pipeline.set_model_bounds("TimeVariableLength", bounds_t)
-    pipeline.set_model_bounds("Const", bounds_c)
-    pipeline.set_model_bounds("Time", bounds_t)
-    pipeline.set_model_x0("Time", [1e-5, 700, 300, 1e-5])
-    pipeline.set_model_x0("Const", [1e-5])
+    pipeline.set_model_bounds("ConstVariable", bounds_c)
+    pipeline.set_model_bounds("TimeVariableLength", bounds_t)
+    pipeline.set_model_x0("TimeVariableLength", [1e-5, 1000, 300, 1e-5])
+    pipeline.set_model_x0("ConstVariable", [1e-5])
     # pipeline.show_rasters()
-    pipeline.fit_even_odd(solver_params=solver_params)
+    # pipeline.fit_even_odd(solver_params=solver_params)
     pipeline.fit_all_models(solver_params=solver_params)
-    pipeline.compare_even_odd("Const", "Time", 0.01)
+    # pipeline.compare_even_odd("ConstVariable", "TimeVariableLength", 0.01)
     # pipeline.compare_even_odd("Const", "Time", 0.01)
-    pipeline.compare_models("Const", "Time", 0.01, smoother_value=100)
-
+    pipeline.compare_models("ConstVariable", "TimeVariableLength", 0.01, smoother_value=100)
 
 
 
@@ -699,7 +744,7 @@ def run_script(cell_range, session):
     # pipeline.compare_models("Const", "Time", 0.01)
     # pipeline.compare_models("Time", "SigmaMuTau", 0.01)
 
-# run_script(range(31,33), "s23")
+# run_script([31, 32], "s23")
 if __name__ == "__main__":
     # session = sys.argv[-3]
     session = "bolkan"
