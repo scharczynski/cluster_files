be426e006c6fa7c216c5fd0767e848ab60613e06
scharczynski <scharcz@brandeis.edu>
2020-04-22 03:23:35 -0400

    added waqrden stim model

--- models.py
+++ models.py
@@ -228,6 +228,67 @@ class SigmaMuTauStimClassRP(Model):
 
         return (np.sum(fun, axis=1)/fun.shape[1])
 
+class SigmaMuTauStimWarden(Model):
+    def __init__(self, data):
+        super().__init__(data)
+        self.param_names = ["sigma", "mu","tau", "a_1", "a_2","a_3", "a_4", "a_0"]
+        self.t = np.tile(self.t, (self.num_trials, 1))
+
+    def info_callback(self):
+        self.stims = self.info["stim_identity"]
+        stim_matrix = np.zeros((self.spikes.shape[0], 4))
+        if self.even_odd_trials == "even":
+            trials  = list(self.stims.keys())[::2]
+        elif self.even_odd_trials == "odd":
+            trials = list(self.stims.keys())[1::2]
+        else:
+            trials  =  list(self.stims.keys())
+        #rossi-pool 1 indexed trials
+        trial_indices = [x-1 for x in list(map(int, trials))]
+
+        self.t = self.t[trial_indices]
+        for trial_num, trial in enumerate(trials):
+            stim_class = self.stims[(trial)]["sample"][0]  
+            if stim_class == 1:
+                stim_matrix[int(trial_num)][:] = [1, 0, 0, 0]
+            elif stim_class == 2:
+                stim_matrix[int(trial_num)][:] = [0, 1, 0, 0]
+            elif stim_class == 3:
+                stim_matrix[int(trial_num)][:] = [0, 0, 1, 0]
+            elif stim_class == 4:
+                stim_matrix[int(trial_num)][:] = [0, 0, 0, 1]
+        self.stim_matrix = stim_matrix
+        return self.stim_matrix
+
+    def model(self, x, plot=False):
+    
+        s, mu, tau, a_1,a_2,a_3, a_4, a_0 = x
+        l = 1/tau
+        fun1 = (np.exp((l/2)*(2*mu+l*s**2-2*self.t))*sse.erfc((mu+l*s**2-self.t)/(np.sqrt(2)*s)))
+
+        fun = (
+            (a_1*(self.stim_matrix[:, 0] * fun1.T))
+            + (a_2*(self.stim_matrix[:, 1] *fun1.T))
+            + (a_3*(self.stim_matrix[:, 2] *fun1.T))
+            + (a_4*(self.stim_matrix[:, 3] *fun1.T))
+        ) + a_0
+
+        return fun
+
+    def objective(self, x):
+        fun = self.model(x).T
+
+        obj = np.sum(self.spikes * (-np.log(fun)) +
+                      (1 - self.spikes) * (-np.log(1 - (fun))))
+
+        return obj
+
+    def plot_model(self, x):
+        fun = self.model(x)
+
+
+        return (np.sum(fun, axis=1)/fun.shape[1])
+
 class SigmaMuTauDual(Model):
     def __init__(self, data):
         super().__init__(data)
